5c5ff9cdd247c8168f5bda0eee17120262796081
ClothifyStore <chanukalankanjana1119@gmail.com>
2024-06-23 05:08:50 +0530

    update

--- src/main/java/edu/icet/demo/controller/AdminDashController.java
+++ src/main/java/edu/icet/demo/controller/AdminDashController.java
@@ -6,10 +6,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.Random;
 import java.util.ResourceBundle;
 
@@ -23,41 +25,23 @@ public class AdminDashController implements Initializable {
     public TextField empNameField;
     public TextField empEmailField;
     public TextField empAddressField;
-    public ComboBox empIdField;
-    public Text empIdText;
-    public Text empIdLabel;
     public Button deleteBtn;
     public Button addEmployeeBtn;
     public Button updateBtn;
     public TableView employeeTable;
+    public TextField empIdField;
 
     UserBoImpl userBoImpl = new UserBoImpl();
     String selectedId;
     SceneSwitchController sceneSwitchController = SceneSwitchController.getInstance();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        empIdField.setVisible(false);
-        empIdField.getSelectionModel().selectedItemProperty().addListener((observableValue,oldValue,newValue) -> {
-
-            User user = userBoImpl.getUserById((String) newValue);
-
-            empNameField.setText(user.getName());
-            empAddressField.setText(user.getAddress());
-            empEmailField.setText(user.getEmail());
-
-            selectedId = (String) newValue;
-
-        });
-
-        empIdText.setText(userBoImpl.generateEmployeeId());
-        updateBtn.setVisible(false);
-        deleteBtn.setVisible(false);
-
         IdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
         NameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         EmailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
         AddressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
 
+        empIdField.setText(userBoImpl.generateEmployeeId());
         employeeTable.setItems(userBoImpl.getAllUsers());
     }
 
@@ -69,7 +53,7 @@ public class AdminDashController implements Initializable {
         String encrypt = Integer.toString(pass);
         String passwprd = userBoImpl.passwordEncrypt(encrypt);
 
-        User user = new User(empIdText.getText(),
+        User user = new User(empIdField.getText(),
                 empNameField.getText(),
                 empEmailField.getText(),
                 passwprd,
@@ -86,7 +70,7 @@ public class AdminDashController implements Initializable {
                 alert.setTitle("Employee Added");
                 alert.setContentText("Employee Added Successfully..!");
                 alert.showAndWait();
-                empIdText.setText(userBoImpl.generateEmployeeId());
+                empIdField.setText(userBoImpl.generateEmployeeId());
                 empAddressField.setText("");
                 empEmailField.setText("");
                 empNameField.setText("");
@@ -97,11 +81,76 @@ public class AdminDashController implements Initializable {
         }
     }
 
-    public void UpdateActionBtn(ActionEvent actionEvent) {
-    }
 
     public void ViewActionBtn(ActionEvent actionEvent) {
+        User user = userBoImpl.getUserById(empIdField.getText());
+                empNameField.setText(user.getName());
+                empEmailField.setText(user.getEmail());
+                empAddressField.setText(user.getAddress());
     }
+
+    public  void clear(){
+        empIdField.setText("");
+        empNameField.setText("");
+        empEmailField.setText("");
+        empAddressField.setText("");
+    }
+
+
+    public void UpdateActionBtn(ActionEvent actionEvent) {
+        if (!empNameField.getText().equals("") && !empEmailField.getText().equals("") && empAddressField.getText().equals("")){
+            User user = new User(empIdField.getText(),
+                    empNameField.getText(),
+                    empEmailField.getText(),
+                    null,
+                    null,
+                    empAddressField.getText());
+
+            boolean isUpdate = userBoImpl.updateUser(user);
+
+            if (isUpdate){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Employee Update");
+                alert.setContentText("Employee Update Successfully");
+                alert.showAndWait();
+                empNameField.setText("");
+                empEmailField.setText("");
+                empAddressField.setText("");
+                employeeTable.setItems(userBoImpl.getAllUsers());
+                clear();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Something Missing");
+            alert.setContentText("Please Check your Form again..!!!");
+            alert.showAndWait();
+        }
+    }
+
+    public void DeleteActionBtn(ActionEvent actionEvent) {
+        if (!empIdField.getText().equals("")){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Deleting");
+            alert.setContentText("Are you sure want to delete this Employee");
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.get()== ButtonType.OK){
+                boolean isDeleted = userBoImpl.deleteUserById(empIdField.getText());
+                if (isDeleted){
+                    Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+                    alert2.setTitle("Employee Deleted");
+                    alert2.setContentText("Employee deleted successfully");
+                    alert2.showAndWait();
+                    employeeTable.setItems(userBoImpl.getAllUsers());
+                    empEmailField.setText("");
+                    empAddressField.setText("");
+                    empNameField.setText("");
+                    clear();
+                }
+            }
+        }
+    }
+
 
     public void ManageEmpBtn(ActionEvent actionEvent) {
     }
@@ -121,9 +170,13 @@ public class AdminDashController implements Initializable {
     public void LogoutBtn(ActionEvent actionEvent) {
     }
 
-    public void DeleteActionBtn(ActionEvent actionEvent) {
+
+    public void releaseEmailkey(KeyEvent keyEvent) {
+        boolean isValidEmail = userBoImpl.isValidEmail(empEmailField.getText());
+        if (!isValidEmail) {
+            addEmployeeBtn.setVisible(true);
+        } else {
+            addEmployeeBtn.setDisable(false);
+        }
     }
-
-
-
 }
