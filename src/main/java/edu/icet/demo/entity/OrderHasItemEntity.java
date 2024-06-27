813a2f5baff749f778f0c30f33a65d2a2859d993
ClothifyStore <chanukalankanjana1119@gmail.com>
2024-06-27 11:39:53 +0530

    update

--- src/main/java/edu/icet/demo/entity/OrderHasItemEntity.java
+++ src/main/java/edu/icet/demo/entity/OrderHasItemEntity.java
@@ -1,4 +1,22 @@
 package edu.icet.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "order_has_item")
+@Entity(name = "order_has_item")
 public class OrderHasItemEntity {
+    private Integer id;
+    private String orderId;
+    private String itemId;
+    private int qty;
+    private double amount;
 }
--- src/main/java/edu/icet/demo/model/OrderHasItem.java
+++ src/main/java/edu/icet/demo/model/OrderHasItem.java
@@ -1,4 +1,18 @@
 package edu.icet.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderHasItem {
+    private Integer id;
+    private String orderId;
+    private String itemId;
+    private int qty;
+    private double amount;
 }
